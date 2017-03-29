c747912a5fec188def9db5954445d8b8f23cb3ac
NirvanAcN <mahaomeng@jingruiwangke.com>
2017-03-29 20:14:36 +0800

    0.0.1 Release

---
+++ JRKit/JRKit.h
@@ -0,0 +1,19 @@
+//
+//  JRKit.h
+//  JRKit
+//
+//  Created by 京睿 on 2017/3/29.
+//  Copyright © 2017年 京睿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JRKit.
+FOUNDATION_EXPORT double JRKitVersionNumber;
+
+//! Project version string for JRKit.
+FOUNDATION_EXPORT const unsigned char JRKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRKit/PublicHeader.h>
+
+
---
+++ JRKit/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  JRKit
+//
+//  Created by 京睿 on 2017/3/29.
+//  Copyright © 2017年 京睿. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    private override init() { }
+    open class func say() {
+        print("hello world")
+    }
+}
