04abd6ad25bb24e72dfb900fb28f63e2d58a2065
NirvanAcN <mahaomeng@jingruiwangke.com>
2017-03-29 20:55:50 +0800

    0.0.1 Release

---
+++ JRKit/Classes/JRKit.swift
@@ -0,0 +1,17 @@
+//
+//  JRKit.swift
+//  JRKit
+//
+//  Created by 京睿 on 2017/3/29.
+//  Copyright © 2017年 京睿. All rights reserved.
+//
+
+import UIKit
+
+class JRKit: NSObject {
+
+    open class func kkkk() {
+        print("go")
+    }
+    
+}
---
+++ JRKit/Classes/Pig.swift
@@ -0,0 +1,15 @@
+//
+//  Pig.swift
+//  JRKit
+//
+//  Created by 京睿 on 2017/3/29.
+//  Copyright © 2017年 京睿. All rights reserved.
+//
+
+import UIKit
+
+open class Pig: NSObject {
+    open class func eat() {
+        print("shi")
+    }
+}
